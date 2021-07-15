971acf93869b03ee4fb6bf85a7daaafdb9b8d045
Onur Yüksekkaya <sosyal@onuryuksekkaya.com>
2021-07-15 23:11:27 +0300

    focus event

--- index.js
+++ index.js
@@ -10,33 +10,54 @@ fetch("https://jsonplaceholder.typicode.com/users/1")
     loginInfo.innerHTML = `${json.username}`;
   });
 
+fetch("https://api.jsonbin.io/v3/b/60ef40fdc1256a01cb6f37ad", {
+  headers: {
+    "X-Master-Key":
+      "$2b$10$WJx4a/x7fH5t4H/dSIoCf.9NRJ6b1j4giOf4aBEOw0OknnCEE9GWG",
+  },
+})
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+
 const loadData = () => {
   row.classList.add("spinner-border");
   fetch("https://jsonplaceholder.typicode.com/todos ")
     .then((response) => response.json())
-    .then((json) => {
-      json.forEach((item) => {
-        row.classList.remove("spinner-border");
-        let foodCard = document.createElement("div");
-        foodCard.classList.add(
-          "food-card",
-          "col-lg-3",
-          "my-3",
-          "mx-2",
-          "text-center"
-        );
-        row.appendChild(foodCard);
-        let foodTitle = document.createElement("div");
-        foodTitle.classList.add("title");
-        foodCard.appendChild(foodTitle);
-        foodTitle.innerHTML = `${item.title}`;
-        let badge = document.createElement("span");
-        badge.classList.add("badge", "rounded-pill", "mb-4", "px-3", "py-2");
-        foodCard.appendChild(badge);
-        badge.classList.add(item.completed ? "bg-success" : "bg-secondary");
-        badge.innerHTML = item.completed ? `Completed` : `Not Completed`;
+    .then((data) => {
+      console.log(data);
+      row.classList.remove("spinner-border");
+      data.map((item) => {
+        row.appendChild(Card(item));
       });
     });
+};
+
+const Card = ({ id, title, completed }) => {
+  let foodCard = document.createElement("div");
+  let foodTitle = document.createElement("div");
+  let badge = document.createElement("span");
+
+  //Card Container
+  foodCard.classList.add(
+    "food-card",
+    "col-lg-3",
+    "my-3",
+    "mx-2",
+    "text-center"
+  );
+  foodCard.setAttribute("id", `card-${id}`);
+  // Card title
+  foodTitle.classList.add("title");
+  foodTitle.innerHTML = title;
+  // card  favorite badge
+  badge.classList.add("badge", "rounded-pill", "mb-4", "px-3", "py-2");
+  badge.classList.add(completed ? "bg-success" : "bg-secondary");
+  badge.innerHTML = completed ? `Favorited` : `Not Favorited`;
+  // add to card containers
+  foodCard.appendChild(foodTitle);
+  foodCard.appendChild(badge);
+
+  return foodCard;
 };
 
 loadData();
