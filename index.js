690e9d7b4e2e4e03d41201cc85a1e5307bedd79e
Onur Yüksekkaya <sosyal@onuryuksekkaya.com>
2021-07-16 02:55:16 +0300

    api change

--- index.js
+++ index.js
@@ -10,52 +10,55 @@ fetch("https://jsonplaceholder.typicode.com/users/1")
     loginInfo.innerHTML = `${json.username}`;
   });
 
-fetch("https://api.jsonbin.io/v3/b/60ef40fdc1256a01cb6f37ad", {
-  headers: {
-    "X-Master-Key":
-      "$2b$10$WJx4a/x7fH5t4H/dSIoCf.9NRJ6b1j4giOf4aBEOw0OknnCEE9GWG",
-  },
-})
-  .then((response) => response.json())
-  .then((data) => console.log(data));
-
 const loadData = () => {
+  // adding spinner to before load data
   row.classList.add("spinner-border");
-  fetch("https://jsonplaceholder.typicode.com/todos ")
+  fetch("https://api.jsonbin.io/v3/b/60ef40fdc1256a01cb6f37ad", {
+    headers: {
+      "X-Master-Key":
+        "$2b$10$WJx4a/x7fH5t4H/dSIoCf.9NRJ6b1j4giOf4aBEOw0OknnCEE9GWG",
+    },
+  })
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
+      //removing spinner to after load data
       row.classList.remove("spinner-border");
-      data.map((item) => {
+      console.log(data.record);
+      data.record.map((item) => {
         row.appendChild(Card(item));
       });
     });
 };
 
-const Card = ({ id, title, completed }) => {
+const Card = ({ idMeal, strMeal, strMealThumb, strYoutube }) => {
   let foodCard = document.createElement("div");
   let foodTitle = document.createElement("div");
-  let badge = document.createElement("span");
+  let imageThumb = document.createElement("img");
+  let youtube = document.createElement("div");
 
   //Card Container
   foodCard.classList.add(
     "food-card",
-    "col-lg-3",
+    "col-lg-4",
+    "col-md-6",
     "my-3",
-    "mx-2",
-    "text-center"
+    "mx-auto"
   );
-  foodCard.setAttribute("id", `card-${id}`);
+  foodCard.setAttribute("id", `card-${idMeal}`);
   // Card title
   foodTitle.classList.add("title");
-  foodTitle.innerHTML = title;
-  // card  favorite badge
-  badge.classList.add("badge", "rounded-pill", "mb-4", "px-3", "py-2");
-  badge.classList.add(completed ? "bg-success" : "bg-secondary");
-  badge.innerHTML = completed ? `Favorited` : `Not Favorited`;
+  foodTitle.innerHTML = strMeal;
+  // Card image
+  imageThumb.classList.add("img-top");
+  imageThumb.src = strMealThumb;
+  // Card youtube link
+  youtube.classList.add("more-btn");
+  youtube.innerHTML =
+    "<a href='" + strYoutube + "' target='_blank'>Watch The Video</a>";
   // add to card containers
+  foodCard.appendChild(imageThumb);
   foodCard.appendChild(foodTitle);
-  foodCard.appendChild(badge);
+  foodCard.appendChild(youtube);
 
   return foodCard;
 };
